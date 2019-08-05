9c9d959bd747d8b80956f8e6721dbca05a170a54
jguptill <josh.guptill@gmail.com>
2019-08-05 14:45:57 -0400

    initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = (lat, long, callback) => {
+    const url = 'https://api.darksky.net/forecast/91f270ecae04095cde3b26b148db3d10/' + lat + ',' + long + '?exclude=minutely,hourly,flags'
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service')
+        } else if (body.error) {
+            callback('Unable to find location.')
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out.  There is a ' + body.currently.precipProbability + '% chance of rain.');
+        }
+    })
+}
+
+module.exports = forecast
---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request');
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +
+        '.json?access_token=pk.eyJ1IjoiZ3VwdGlsbDc4IiwiYSI6ImNqdnduenpxNjRlanM0OXFydHA3ODZlNTYifQ.T0hkZT26yr2sqoiSFN6vrA&limit=1'
+    request({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to location services', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location, try another search', undefined)
+        } else {
+            const data = body.features[0];
+            callback(undefined, {
+                latitude: data.center[1],
+                longitude: data.center[0],
+                location: data.place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
